d0f198c58908b644fcad2fb71059a9cbc7aa19bd
Nick F <nick@nick.gs>
2014-01-21 21:51:39 -0800

    Wrong file name lol

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -12,7 +12,7 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.*;
 
 public class HelloWorld extends HttpServlet {
-    @Override
+   
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 	throws ServletException, IOException {
         resp.getWriter().print("Hello from Java!\n");
