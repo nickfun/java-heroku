69b86e0f782e9373c0d190c370bafe5c11a83924
Nick F <nick@nick.gs>
2014-01-23 01:02:52 -0800

    Adding Gson to do JSON output, but it wont build yet

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,6 +3,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import org.eclipse.jetty.servlet.*;
 
+import com.google.code.gson.*
+
 public class Test extends HttpServlet {
 
     protected void doGet( HttpServletRequest req, HttpServletResponse resp )
@@ -11,7 +13,10 @@ public class Test extends HttpServlet {
 	Obj t = new Obj();
 	t.setName("Nick");
 	t.setAge(12);
-	resp.getWriter().print("Obj: " + t);
+       	//resp.getWriter().print("Obj: " + t);
+	resp.setHeader("Content-Type", "application/json");
+	Gson jsonWriter = new Gson();
+	resp.getWriter().print(  jsonWriter.toJson(t) );
     }
 
     public Test() {
