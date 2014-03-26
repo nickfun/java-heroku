1587ea0441542315fb87de701380d69177306d4a
Nick Fun <nick@nick.gs>
2014-03-26 16:02:30 -0700

    Have to tell Maven to use java 1.7 instead of 1.3 (!??!)

--- src/main/java/SystemList.java
+++ src/main/java/SystemList.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.ArrayList;
 
 import com.google.gson.*;
 
@@ -9,20 +10,26 @@ import org.eclipse.jetty.servlet.*;
 
 public class SystemList extends HttpServlet {
 
-    private ArrayList<Object> doQuery() {
+    private ArrayList<String> doQuery() {
 	String dbUrl = "jdbc:mysql://"+System.getenv("DB_HOST")+"/"+System.getenv("DB_NAME");
 	String dbClass = "com.mysql.jdbc.Driver";
 	String query = "select * from systems order by company, name;";
-	try {
-	    Class.forName(dbClass);
-	    Connection conn = DriverManager.getConnect(dbUrl, System.getenv("DB_USER"), System.getenv("DB_PASS"));
-	    Statement smt = conn.createStatement();
-	    ResultSet sqlresults = smt.executeQuery(query);
-    }
+	
+	Class.forName(dbClass);
+	Connection conn = DriverManager.getConnect(dbUrl, System.getenv("DB_USER"), System.getenv("DB_PASS"));
+	Statement smt = conn.createStatement();
+	ResultSet sqlresults = smt.executeQuery(query);
+	ArrayList<String> systemNames = new ArrayList<String>();
+	while( sqlresults.next() ) {
+	    systemNames.push( sqlresults.getString("company") + " " + sqlresults.getString("name") );
+	}
+	return systemNames;
+    }    
 
     protected void doGet( HttpServletRequest req, HttpServletResponse resp )
 	throws ServletException, IOException
     {
-	
+	resp.getWriter().print("Hello!");
+    }
 
 }
