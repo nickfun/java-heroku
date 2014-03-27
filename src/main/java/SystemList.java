a5b0889a82c639dbbeb550bf0aad12ba0072cf0c
Nick Fun <nick@nick.gs>
2014-03-27 10:28:39 -0700

    Adding in debugging & exception catching. I am not setting up my connection correctly.

--- src/main/java/SystemList.java
+++ src/main/java/SystemList.java
@@ -11,6 +11,7 @@ import org.eclipse.jetty.servlet.*;
 public class SystemList extends HttpServlet {
 
     private ArrayList<String> doQuery() throws ClassNotFoundException, SQLException {
+	System.out.println("SystemList::doQuery");
 	String dbUrl = "jdbc:mysql://"+System.getenv("DB_HOST")+"/"+System.getenv("DB_NAME");
 	String dbClass = "com.mysql.jdbc.Driver";
 	String query = "select * from systems order by company, name;";
@@ -21,8 +22,11 @@ public class SystemList extends HttpServlet {
 						   System.getenv("DB_USER"), 
 						   System.getenv("DB_PASS")
 	);
+	System.out.println("Conn is established");
 	Statement smt = conn.createStatement();
+	System.out.println("Statement created");
 	ResultSet sqlresults = smt.executeQuery(query);
+	System.out.println("SystemList, executQuery finished");
 	ArrayList<String> systemNames = new ArrayList<String>();
 	while( sqlresults.next() ) {
 	    systemNames.add( sqlresults.getString("company") + " " + sqlresults.getString("name") );
@@ -31,9 +35,24 @@ public class SystemList extends HttpServlet {
     }    
 
     protected void doGet( HttpServletRequest req, HttpServletResponse resp )
-	throws ServletException, IOException
+	throws ServletException, IOException 
     {
+	ArrayList<String> gameNames;
+	resp.setContentType("text/html");
 	resp.getWriter().print("Hello!");
+	try {
+	    gameNames = doQuery();
+	} catch( ClassNotFoundException e ) {
+	    gameNames = new ArrayList<String>();
+	    gameNames.add("Query failed");
+	} catch( SQLException e ) {
+	    gameNames = new ArrayList<String>();
+	    gameNames.add("SQL Error " + e.getErrorCode());
+	    gameNames.add("msg: " + e.getMessage());
+	}
+	for( String name : gameNames ) {
+	    resp.getWriter().print("<br>" + name);
+	}
     }
 
 }
