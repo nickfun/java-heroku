1b3d1f0f4749d7b7a92cbce7ff2858f2d745e718
Nick Fun <nick@nick.gs>
2014-04-30 13:37:16 -0700

    /systems path prints a list of system names. need to make it json

--- src/main/java/gs/nick/server/SystemList.java
+++ src/main/java/gs/nick/server/SystemList.java
@@ -6,6 +6,10 @@ import java.util.ArrayList;
 import com.google.gson.*;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import org.eclipse.jetty.servlet.*;
@@ -13,48 +17,81 @@ import org.eclipse.jetty.servlet.*;
 public class SystemList extends HttpServlet {
 
     private ArrayList<String> doQuery() throws ClassNotFoundException, SQLException {
-	System.out.println("SystemList::doQuery");
-	String dbUrl = "jdbc:mysql://"+System.getenv("DB_HOST")+"/"+System.getenv("DB_NAME");
-	String dbClass = "com.mysql.jdbc.Driver";
-	String query = "select * from systems order by company, name;";
-	
-	Class.forName(dbClass);
-	Connection conn = DriverManager.getConnection(
-						   dbUrl, 
-						   System.getenv("DB_USER"), 
-						   System.getenv("DB_PASS")
-	);
-	System.out.println("Conn is established");
-	Statement smt = conn.createStatement();
-	System.out.println("Statement created");
-	ResultSet sqlresults = smt.executeQuery(query);
-	System.out.println("SystemList, executQuery finished");
-	ArrayList<String> systemNames = new ArrayList<String>();
-	while( sqlresults.next() ) {
-	    systemNames.add( sqlresults.getString("company") + " " + sqlresults.getString("name") );
-	}
-	return systemNames;
-    }    
+        System.out.println("SystemList::doQuery");
+        String dbUrl = "jdbc:mysql://" + System.getenv("DB_HOST") + "/" + System.getenv("DB_NAME");
+        String dbClass = "com.mysql.jdbc.Driver";
+        String query = "select * from systems order by company, name;";
 
-    protected void doGet( HttpServletRequest req, HttpServletResponse resp )
-	throws ServletException, IOException 
+        Class.forName(dbClass);
+        Connection conn = DriverManager.getConnection(
+                dbUrl,
+                System.getenv("DB_USER"),
+                System.getenv("DB_PASS")
+        );
+        System.out.println("Conn is established");
+        Statement smt = conn.createStatement();
+        System.out.println("Statement created");
+        ResultSet sqlresults = smt.executeQuery(query);
+        System.out.println("SystemList, executQuery finished");
+        ArrayList<String> systemNames = new ArrayList<String>();
+        while (sqlresults.next()) {
+            systemNames.add(sqlresults.getString("company") + " " + sqlresults.getString("name"));
+        }
+        return systemNames;
+    }
+
+    private Connection getConnection() {
+        Connection connection = null;
+        String user, pass, url, answer;
+        URI dbUri;
+        try {
+            dbUri = new URI(System.getenv("DATABASE_URL"));
+        } catch (URISyntaxException ex) {
+            return null;
+        }
+
+        user = dbUri.getUserInfo().split(":")[0];
+        pass = dbUri.getUserInfo().split(":")[1];
+        url = "jdbc:" + dbUri.getScheme() + "://" + dbUri.getHost() + dbUri.getPath();
+
+        String query = "SELECT name FROM systems WHERE id=10;";
+        String dbClass = "com.mysql.jdbc.Driver";
+
+        try {
+            Class.forName(dbClass);
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException 
     {
-	ArrayList<String> gameNames;
-	resp.setContentType("text/html");
-	resp.getWriter().print("Hello!");
-	try {
-	    gameNames = doQuery();
-	} catch( ClassNotFoundException e ) {
-	    gameNames = new ArrayList<String>();
-	    gameNames.add("Query failed");
-	} catch( SQLException e ) {
-	    gameNames = new ArrayList<String>();
-	    gameNames.add("SQL Error " + e.getErrorCode());
-	    gameNames.add("msg: " + e.getMessage());
-	}
-	for( String name : gameNames ) {
-	    resp.getWriter().print("<br>" + name);
-	}
+        try {
+            Connection conn = getConnection();
+            if (conn == null) {
+                resp.getWriter().println("DB FAIL");
+            }
+            ArrayList<String> systemNames = new ArrayList<>();
+            resp.setContentType("text/html");
+            resp.getWriter().print("Hello!");
+            PreparedStatement stm = conn.prepareStatement("SELECT * FROM systems;");
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                systemNames.add( rs.getString("name") );
+            }
+            resp.getWriter().print( systemNames.toString() );
+        } catch (SQLException ex) {
+            Logger.getLogger(SystemList.class.getName()).log(Level.SEVERE, null, ex);
+            resp.getWriter().print("DB Error");
+            return;
+        }
+        
     }
 
 }
