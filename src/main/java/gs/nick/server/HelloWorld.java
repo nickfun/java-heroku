c861f3fcdd506fd33eda35fec5e6f21ff612bcab
Nick Fun <nick@nick.gs>
2014-05-14 17:33:37 -0700

    Prints a kind of index thing"

--- src/main/java/gs/nick/server/HelloWorld.java
+++ src/main/java/gs/nick/server/HelloWorld.java
@@ -13,11 +13,22 @@ import javax.servlet.http.*;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.*;
 
+/**
+ * This handles the root resources on the server
+ */
 public class HelloWorld extends HttpServlet {
    
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 	throws ServletException, IOException {
-        resp.getWriter().print("Hello from Java!\n");
+	String msg = 
+	    "\n HelloWorld  /*" +
+	    "\n Name  /name" +
+	    "\n Letters  /letters" +
+	    "\n Test  /test" +
+	    "\n SystemList  /systems";
+	String output = "<html><body><h1>Hello welcome to <code>server</code></h1><pre>" + msg + "</pre></body></html";
+	resp.getWriter().print(output);
+	System.out.println("served /");
     }
 
     public HelloWorld() {
